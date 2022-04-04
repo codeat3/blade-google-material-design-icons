79480fc84d94a88dcca850c89030dfd5d4393bcc
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-04-04 17:01:35 +0000

    Fix styling

--- src/BladeGoogleMaterialDesignIconsServiceProvider.php
+++ src/BladeGoogleMaterialDesignIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeGoogleMaterialDesignIconsServiceProvider extends ServiceProvide
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-google-material-design-icons', []);
 
-            $factory->add('google-material-design-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('google-material-design-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-google-material-design-icons.php', 'blade-google-material-design-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-google-material-design-icons.php', 'blade-google-material-design-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-google-material-design-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-google-material-design-icons'),
             ], 'blade-gmdi'); // TODO: updating this alias to `blade-google-material-design-icons` in next major release
 
             $this->publishes([
-                __DIR__.'/../config/blade-google-material-design-icons.php' => $this->app->configPath('blade-google-material-design-icons.php'),
+                __DIR__ . '/../config/blade-google-material-design-icons.php' => $this->app->configPath('blade-google-material-design-icons.php'),
             ], 'blade-google-material-design-icons-config');
         }
     }
